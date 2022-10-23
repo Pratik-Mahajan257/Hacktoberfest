1c2ec948fd9465afb2bcbacc3a98e43140876694
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2022-10-23 09:29:47 +0530

    fixed the class name of the file

--- Languages/Java/GCD_Recursive.java
+++ Languages/Java/GCD_Recursive.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.*;
 
-public class GCD_recursive {
+public class GCD_Recursive {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
